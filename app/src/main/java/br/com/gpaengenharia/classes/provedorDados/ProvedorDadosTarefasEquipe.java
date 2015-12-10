f031207baa8951aa73cc45a43c917dd657841b3f
Jorgeley <jorgeley@gmail.com>
2015-12-10 21:36:53 -0200

    Update ProvedorDadosTarefasEquipe.java

--- app/src/main/java/br/com/gpaengenharia/classes/provedorDados/ProvedorDadosTarefasEquipe.java
+++ app/src/main/java/br/com/gpaengenharia/classes/provedorDados/ProvedorDadosTarefasEquipe.java
@@ -12,8 +12,8 @@ import br.com.gpaengenharia.activities.AtvLogin;
 import br.com.gpaengenharia.classes.xmls.XmlTarefasEquipe;
 
 /**
- Monta TreeMap de beans <Projeto, List<Tarefa>>
- herda de ProvedorDados e implementa ProvedorDadosInterface
+ build TreeMap of beans <Projeto, List<Tarefa>>
+ inherit from ProvedorDados and implements ProvedorDadosInterface
   */
 public class ProvedorDadosTarefasEquipe extends ProvedorDados implements ProvedorDadosInterface {
     private Context contexto;
